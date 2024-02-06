57d6af7859f6fade2af62a433db2c550de83e118
Abe Omaits <aomaits@gmail.com>
2024-02-06 14:59:28 -0600

    initial setup and code along with tutorial

---
+++ thesaurus/src/App.js
@@ -0,0 +1,22 @@
+import {useState} from 'react';
+import './App.css';
+
+function App() {
+  const [word, setWord] = useState('');
+
+  return (
+    <div className="App">
+      <form>
+        <label htmlFor="word-input">Your Word Here:</label>
+        <input
+          value={word}
+          onChange={(e) => setWord(e.target.value)}
+          id="word-input"
+        ></input>
+        <button>Submit</button>
+      </form>
+    </div>
+  );
+}
+
+export default App;
