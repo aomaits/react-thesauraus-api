ea9015b94ec467640188101df5bf4424f9e66432
Abe Omaits <aomaits@gmail.com>
2024-02-07 10:50:45 -0600

    made a few styling changes, API call had been added to state but is now mapping and rendering list items to page

--- thesaurus/src/App.js
+++ thesaurus/src/App.js
@@ -1,22 +1,22 @@
-import {useState} from 'react';
+import { useState } from 'react';
 import './App.css';
 import axios from 'axios'; // Import Axios
 
 function App() {
   const [word, setWord] = useState('');
-  const [synonyms, setSynonyms] =useState({})
+  const [synonyms, setSynonyms] = useState([]); // Initialize as empty array
 
   const handleFetchSynonyms = async (e) => {
     e.preventDefault();
 
     try {
-      const response = await axios.get('https://api.datamuse.com/words?rel_syn=fast', {
-      params: {
-        rel_syn: word // Passing in word as the query parameter
-      }
-    });
+      const response = await axios.get(`https://api.datamuse.com/words?rel_syn=${word}`, {
+        params: {
+          rel_syn: word // Passing in word as the query parameter
+        }
+      });
 
-    setSynonyms(response.data); // Setting synonyms w/ API response data
+      setSynonyms(response.data); // Setting synonyms w/ API response data
     } catch (error) {
       console.error('Error fetching synonyms: ', error);
     }
@@ -27,15 +27,23 @@ function App() {
 
   return (
     <div className="App">
-      <form onSubmit={handleFetchSynonyms}>
-        <label htmlFor="word-input">Your Word Here:</label>
-        <input
-          value={word}
-          onChange={(e) => setWord(e.target.value)}
-          id="word-input"
-        ></input>
-        <button>Submit</button>
-      </form>
+      <div>
+        <form onSubmit={handleFetchSynonyms}>
+          <label htmlFor="word-input">Your Word Here:</label>
+          <input
+            value={word}
+            onChange={(e) => setWord(e.target.value)}
+            id="word-input"
+          ></input>
+          <button>Submit</button>
+        </form>
+
+        <ul>
+          {synonyms.length > 0 && synonyms.map((synonym) => {
+            return <li key={synonym.word}>{synonym.word}</li>
+          })}
+        </ul>
+      </div>
     </div>
   );
 }
