669343f6268c2875575d48522157c6a8003c0519
Abe Omaits <aomaits@gmail.com>
2024-02-07 10:17:25 -0600

    followed tutorial but had to adjust API to be functional, now successfully calling API and setting results to state

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # react-thesauraus-api
 practicing react, using tutorial to access thesaurus api
+
+due to compatability issues with Webpack 4 vs webpack 5, used axios to make the API call instead (ChatGPT helped find this alternative)
--- thesaurus/src/App.js
+++ thesaurus/src/App.js
@@ -1,12 +1,33 @@
 import {useState} from 'react';
 import './App.css';
+import axios from 'axios'; // Import Axios
 
 function App() {
   const [word, setWord] = useState('');
+  const [synonyms, setSynonyms] =useState({})
+
+  const handleFetchSynonyms = async (e) => {
+    e.preventDefault();
+
+    try {
+      const response = await axios.get('https://api.datamuse.com/words?rel_syn=fast', {
+      params: {
+        rel_syn: word // Passing in word as the query parameter
+      }
+    });
+
+    setSynonyms(response.data); // Setting synonyms w/ API response data
+    } catch (error) {
+      console.error('Error fetching synonyms: ', error);
+    }
+    // fetch()
+    // .then((response) => response.json())
+    // .then(setSynonyms);
+  }
 
   return (
     <div className="App">
-      <form>
+      <form onSubmit={handleFetchSynonyms}>
         <label htmlFor="word-input">Your Word Here:</label>
         <input
           value={word}
